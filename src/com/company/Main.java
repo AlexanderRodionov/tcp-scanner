27ebe0e39030c88a75ffb699b13a3425d4ba2051
AlexanderRodionov <dobr.14@mail.ru>
2021-05-21 19:16:40 +0300

    scan open ports

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Socket s;
+
+        for (int i = 0; i < 65536; i++) {
+
+            try {
+                s = new Socket(args[0], i);
+                System.out.println("Port open " + i);
+
+            } catch (IOException e) {
+                //System.out.println("err");
+            }
+        }
+
     }
 }

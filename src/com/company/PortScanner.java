a372597a4c0802f3f124879785b6d8a9984f1fc0
AlexanderRodionov <dobr.14@mail.ru>
2021-05-22 16:52:59 +0300

    add queue and executor

---
+++ src/com/company/PortScanner.java
@@ -0,0 +1,52 @@
+package com.company;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+//import org.slf4j.Logger;
+
+public class PortScanner {
+    private static final Logger logger = LoggerFactory.getLogger(PortScanner.class);
+    private int timeOut = 100;
+    private int poolSize = 1;
+
+
+
+    public void start(String[] hosts, String[] ports, int otherPoolSize, List<Result> results) {
+        System.out.println("Start scanning...");
+        logger.info("Start scanning...");
+
+        poolSize = otherPoolSize;
+
+        LinkedBlockingQueue<Runnable> queue = new LinkedBlockingQueue<>();
+
+        for (String host : hosts) {
+            for (String port : ports) {
+                if(port.contains("-")) {
+                    int port1 = Integer.parseInt(port.substring(0, port.indexOf("-")));
+                    int port2 = Integer.parseInt(port.substring(port.lastIndexOf("-") +1, ports.length));
+                    for (int i = port1; i < port2; i++) {
+                        queue.put(new ScanThread(host, Integer.toString(i), timeOut, results));
+                    }
+                }
+                else {
+                    queue.put(new ScanThread(host, port, timeOut, results));
+                }
+            }
+        }
+
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(poolSize, poolSize, timeOut,
+                TimeUnit.MILLISECONDS, queue);
+
+        executor.prestartAllCoreThreads();
+        while (executor.getTaskCount() != executor.getCompletedTaskCount()) {
+
+        }
+
+        logger.info("Completed task count " + executor.getCompletedTaskCount());
+        System.out.println("Completed task count " + executor.getCompletedTaskCount());
+
+        executor.shutdown();
+    }
+}
